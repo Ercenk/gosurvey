9b217bb8315e705885cb16dc75391900fa79b868
cdemircioglu <cdemircioglu@gmail.com>
2015-06-03 22:32:26 -0700

    Blob to HTML change

--- getCounter.php
+++ getCounter.php
@@ -6,7 +6,7 @@
         $www_root = 'http://gurbetinoylari.azurewebsites.net';
         $containerName = "gurbetoylaritutanakmetadata";
 		
-		$blobUrl = "http://abacusdms.blob.core.windows.net/gurbetoylaritutanakmetadata/metadata/tutanakcounter?sv=2014-02-14&sr=c&sig=mKhOvF3w8B3GqB6AQtiKKXC7LSwuckpdUUnSfWgTqBE%3D&st=2015-06-03T07%3A00%3A00Z&se=2016-12-14T08%3A00%3A00Z&sp=r";
+		$blobUrl = "http://abacusdms.blob.core.windows.net/gurbetoylaritutanakmetadata/metadata/tutanakcounter.html?sv=2014-02-14&sr=c&sig=NKf%2BaBw%2F39YLSWiLvXrGaLdbI4ZSD2hqqCjqhacZdTw%3D&st=2015-06-03T07%3A00%3A00Z&se=2015-10-16T07%3A00%3A00Z&sp=r";
 		$theData = file_get_contents($blobUrl);
 		echo $theData;
 ?>
